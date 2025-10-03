18ea7aef86249f1c77b443b5e428e99cad1ec0b3
WARLLEY RAMON CRUZ ROCHA <warlley_rocha@estudante.sesisenai.org.br>
2025-10-03 11:19:03 -0300

    aula 2: variaveis e tipos

---
+++ aula2.py
@@ -0,0 +1,26 @@
+#Trabahando Com Variáveis
+
+#definindo uma variável com meu nome
+nome = "ramon"
+print("o nome é", nome)
+
+palavra = "Xulofímpola"
+letra = "A"
+quantidade = 10
+valor = 10.5
+aberto = False
+
+print("0 valor de palavra é", palavra,end="")
+print("(tipo: ",type(palavra, end=""))
+
+print("o valor de letra é", letra, end="")
+print("( tipo: ",type(letra),end="")
+
+print("o valor de quantidade é", quantidade, end="")
+("(tipo: ",type(quantidade),")")
+
+print("o valor de valor é", valor, end="")
+print("( tipo: ",type(valor),")")
+
+print("o valor de aberto é", aberto, end="")
+print("( tipo: ",type(aberto),")")
