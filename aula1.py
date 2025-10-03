1fe99a2e4ba0628333e787d597d1af67451fcd0d
WARLLEY RAMON CRUZ ROCHA <warlley_rocha@estudante.sesisenai.org.br>
2025-10-03 09:48:41 -0300

    aula1: comando print

---
+++ aula1.py
@@ -0,0 +1,11 @@
+# isso é um comentário
+#vamos escapar da maldição
+print("olá mundo!")
+
+print("esse é o curso de",end=" ")
+
+print("lógica","de","programação")
+#podemos mudar o comportamento de finalização
+print("frase","com","hífem",sep="-")
+
+
